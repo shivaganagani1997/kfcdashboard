cbd6ba4f1250d63d1a8702f9a221ee57b2e86fc2
shiva <99701266+shivaganagani1997@users.noreply.github.com>
2022-11-18 11:51:31 +0530

    first commit

---
+++ orders.js
@@ -0,0 +1,38 @@
+const Orders = [
+    {
+        productName:'Chicken Burger',
+        productNumber:'65341',
+        paymentStatus:'Due',
+        shipping:'Pending'
+    },
+    {
+        productName:'Pizza',
+        productNumber:'65342',
+        paymentStatus:'Refunded',
+        shipping:'Declined'
+    },
+    {
+        productName:'Chicken Faham',
+        productNumber:'65343',
+        paymentStatus:'Due',
+        shipping:'Pending'
+    },
+    {
+        productName:'Chicken Fries',
+        productNumber:'65344',
+        paymentStatus:'Paid',
+        shipping:'Delivered'
+    },
+    {
+        productName:'Chicken Lollipop',
+        productNumber:'65345',
+        paymentStatus:'Paid',
+        shipping:'Delivered'
+    },
+    {
+        productName:'Chicken Biryani',
+        productNumber:'65346',
+        paymentStatus:'Due',
+        shipping:'Pending'
+    },
+]
